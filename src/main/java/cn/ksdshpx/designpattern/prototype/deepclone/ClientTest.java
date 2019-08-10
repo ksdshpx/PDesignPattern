b92f16e30a19b3e94660655cbb47857a7adb07b1
Unknown <southeast_px@163.com>
2019-08-10 17:28:16 +0800

    深拷贝实现方式一:利用clone方法！

---
+++ src/main/java/cn/ksdshpx/designpattern/prototype/deepclone/ClientTest.java
@@ -0,0 +1,25 @@
+package cn.ksdshpx.designpattern.prototype.deepclone;
+
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2019/8/10
+ * Time: 11:46
+ * Description:客户端测试
+ */
+public class ClientTest {
+    public static void main(String[] args) throws Exception {
+        Sheep sheep = new Sheep("Tom", 1, "白色");
+        sheep.setFriend(new Sheep("Jack", 2, "黑色"));
+        Sheep sheep2 = (Sheep) sheep.clone();
+        Sheep sheep3 = (Sheep) sheep.clone();
+        Sheep sheep4 = (Sheep) sheep.clone();
+        Sheep sheep5 = (Sheep) sheep.clone();
+        System.out.println(sheep + ",sheep.friend:" + sheep.getFriend().hashCode());
+        System.out.println(sheep2 + ",sheep2.friend:" + sheep2.getFriend().hashCode());
+        System.out.println(sheep3 + ",sheep3.friend:" + sheep3.getFriend().hashCode());
+        System.out.println(sheep4 + ",sheep4.friend:" + sheep4.getFriend().hashCode());
+        System.out.println(sheep5 + ",sheep5.friend:" + sheep5.getFriend().hashCode());
+    }
+}
---
+++ src/main/java/cn/ksdshpx/designpattern/prototype/deepclone/Sheep.java
@@ -0,0 +1,77 @@
+package cn.ksdshpx.designpattern.prototype.deepclone;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2019/8/10
+ * Time: 11:44
+ * Description:羊实体
+ */
+public class Sheep implements Cloneable {
+    private String name;
+    private Integer age;
+    private String color;
+    private String address = "蒙古羊";
+    private Sheep friend;
+
+    public Sheep() {
+    }
+
+    public Sheep(String name, Integer age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Sheep getFriend() {
+        return friend;
+    }
+
+    public void setFriend(Sheep friend) {
+        this.friend = friend;
+    }
+
+    @Override
+    public String toString() {
+        return "Sheep{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Object deep = super.clone();
+        Sheep sheep = (Sheep) deep;
+        if (sheep.getFriend() != null) {
+            sheep.setFriend((Sheep) sheep.getFriend().clone());
+        }
+        return sheep;
+    }
+}
